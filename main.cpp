13128949fd6a6ef7b4e6264e9715f6c78ee10f1f
kh1dr1 <1rd1hk@proton.me>
2024-09-26 10:57:08 +0300

    Atliktas pirmas uzduotis

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
+void printStudentInfo()
+{
+    cout << "Studento vardas: Ilia\n";
+    cout << "Studento studiju programa: Programu Sistemos\n";
+    cout << "Studento argumentas: \"Pasirinkau Programu Sistemu studiju programa, nes noreciau tapti programuotoju.\"\n";
+}
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    printStudentInfo();
+    printStudentInfo();
     return 0;
 }
