eb4294e8f469016d1461af3fdcc8f077aedf0be3
kh1dr1 <1rd1hk@proton.me>
2024-09-26 11:17:43 +0300

    Atliktas 3-ias uzduotis

--- main.cpp
+++ main.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-void printStudentInfo()
+void student()
 {
     cout << "Studento vardas: Ilia\n";
     cout << "Studento studiju programa: Programu Sistemos\n";
     cout << "Studento argumentas: \"Pasirinkau Programu Sistemu studiju programa, nes noreciau tapti programuotoju.\"\n";
 }
 
-void printViko()
+void viko()
 {
     cout << "Vilniaus kolegija\n";
 }
 
-void printEIF()
+void eif()
 {
     cout << "Elektronikos ir informatikos fakultetas\n";
+}
+
+int random_number(int min, int max)
+{
+    return rand() % (max - min + 1) + min;
 }
 
 int main()
 {
     cout << "Uzduotis 1:\n";
-    printStudentInfo();
-    printStudentInfo();
+    student();
+    student();
 
     cout << "\nUzduotis 2:\n";
-    printViko();
-    printEIF();
+    viko();
+    eif();
+
+    cout << "\nUzduotis 3:\n";
+    for (int i = 0; i < 10; ++i)
+    {
+        cout << "Atsitiktinis skaicius #" << i + 1 << " yra: " << random_number(10, 20) << '\n';
+    }
 
     return 0;
 }
